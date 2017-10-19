49dd59d9417c878ef8247bb9c309d0bf413969f5
Roman <romanbondar@users.noreply.github.com>
2017-10-19 23:20:35 +0300

    Update image.php
    Removed HTML code from translation function.

--- image.php
+++ image.php
@@ -57,8 +57,8 @@ get_header();
 				<!-- .entry-content --></div>
 
 				<nav role="navigation" id="image-navigation" class="image-navigation">
-					<div class="nav-previous"><?php previous_image_link( false, __( '<span class="meta-nav"><i class="ico-left-open"></i></span> Previous', 'hiero' ) ); ?></div>
-					<div class="nav-next"><?php next_image_link( false, __( 'Next <span class="meta-nav"><i class="ico-right-open"></i></span>', 'hiero' ) ); ?></div>
+					<div class="nav-previous"><?php previous_image_link( false, '<span class="meta-nav"><i class="ico-left-open"></i></span> ' . __( 'Previous', 'hiero' ) ); ?></div>
+					<div class="nav-next"><?php next_image_link( false, __( 'Next', 'hiero' ) . ' <span class="meta-nav"><i class="ico-right-open"></i></span>' ); ?></div>
 				<!-- #image-navigation --></nav>
 
 				<footer class="entry-meta">
